7906fc9614fee0eb8b02e8237327b566a50827e5
SudhanshuPatel <sudhanshupatel0207@gmail.com>
2024-01-03 15:57:27 +0530

    middlewares auth

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -145,7 +145,7 @@ exports.login = async (req, res) => {
       const payload={
          email: user.email, 
          id: user._id, 
-         role: user.role, 
+         accountType: user.accountType,//!
         }
       const token = jwt.sign(payload, process.env.JWT_SECRET,
         {
