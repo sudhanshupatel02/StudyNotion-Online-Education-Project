31891a146b4cc438ebe8d1504093872cf55c0bef
SudhanshuPatel <sudhanshupatel0207@gmail.com>
2024-01-13 19:28:20 +0530

    chenge User=user

--- server/models/User.js
+++ server/models/User.js
@@ -73,4 +73,4 @@ const userSchema = new mongoose.Schema(
 );
 
 // Export the Mongoose model for the user schema, using the name "user"
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("user", userSchema);
